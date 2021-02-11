2333ba8bfae3b25121e3521495b93ce0024de92d
Andre Hochi <andrehochiwork@gmail.com>
2021-02-11 00:26:30 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,12 +2,6 @@
 
 describe "./calculator.rb" do
 
-first_number = 4
-second_number = 2
-difference = 2
-product = 8
-quotient = (1/2)
-
   it "contains a local variable called first_number that is assigned to a number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
 
